6dd4e23aef407514c5e066eea844bd7c1e381eee
0324bence <d.bence0324@gmail.com>
2024-11-05 18:45:16 +0100

    Implement side switching

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const input = document.querySelector("input");
-const button = document.querySelector("button");
+const button = document.querySelector("#print-button");
+const swap = document.querySelector("#swap-button");
 const output = document.querySelector("#output");
+
+let data = [];
+
+let swapState = false;
 
 input.addEventListener("input", function () {
     if (input.value.length > 0) {
@@ -15,27 +20,8 @@ input.addEventListener("input", function () {
         var reader = new FileReader();
         reader.readAsText(file, "UTF-8");
         reader.onload = function (evt) {
-            const data = evt.target.result.trim().split("\n");
-            const table = document.createElement("table");
-            for (let i = 0; i < data.length; i++) {
-                const row = document.createElement("tr");
-                const columns = data[i].split(";");
-                const col1 = document.createElement("td");
-                const col2 = document.createElement("td");
-                const col3 = document.createElement("td");
-                col1.classList.add("col1");
-                col2.classList.add("col2");
-                col3.classList.add("col3");
-                col1.textContent = columns[0];
-                col2.textContent = "-";
-                col3.textContent = columns[1];
-                row.appendChild(col1);
-                row.appendChild(col2);
-                row.appendChild(col3);
-                table.appendChild(row);
-            }
-            output.innerHTML = "";
-            output.appendChild(table);
+            data = evt.target.result.trim().split("\n");
+            display(swapState);
             // document.write(data);
         };
         reader.onerror = function (evt) {
@@ -47,3 +33,32 @@ input.addEventListener("input", function () {
 button.addEventListener("click", function () {
     print();
 });
+
+swap.addEventListener("click", function () {
+    swapState = !swapState;
+    display(swapState);
+});
+
+function display(reverse) {
+    const table = document.createElement("table");
+    for (let i = 0; i < data.length; i++) {
+        const row = document.createElement("tr");
+        const columns = data[i].split(";");
+        const col1 = document.createElement("td");
+        const col2 = document.createElement("td");
+        const col3 = document.createElement("td");
+        col1.classList.add("col1");
+        col2.classList.add("col2");
+        col3.classList.add("col3");
+
+        col1.textContent = columns[reverse ? 1 : 0];
+        col2.textContent = "-";
+        col3.textContent = columns[reverse ? 0 : 1];
+        row.appendChild(col1);
+        row.appendChild(col2);
+        row.appendChild(col3);
+        table.appendChild(row);
+    }
+    output.innerHTML = "";
+    output.appendChild(table);
+}
