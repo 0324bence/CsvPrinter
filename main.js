a1b3778f1e2b60fe45d5934508c370cbc268a42d
0324bence <d.bence0324@gmail.com>
2025-02-12 21:50:58 +0100

    make font size even smaller

--- main.js
+++ main.js
@@ -45,16 +45,20 @@ function display(reverse) {
     for (let i = 0; i < data.length; i++) {
         const row = document.createElement("tr");
         const columns = data[i].split(";");
+        const col0 = document.createElement("td");
         const col1 = document.createElement("td");
         const col2 = document.createElement("td");
         const col3 = document.createElement("td");
+        col0.classList.add("col0");
         col1.classList.add("col1");
         col2.classList.add("col2");
         col3.classList.add("col3");
 
+        col0.textContent = i + 1 + ".";
         col1.textContent = columns[reverse ? 1 : 0];
         col2.textContent = "-";
         col3.textContent = columns[reverse ? 0 : 1];
+        row.appendChild(col0);
         row.appendChild(col1);
         row.appendChild(col2);
         row.appendChild(col3);
